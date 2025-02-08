02a4d8ae21dda094868aea1674a99c2e60dec65f
wlwljq <1270481922@qq.com>
2025-02-08 12:36:58 +0800

    first commit

---
+++ filelist.v
@@ -0,0 +1,15 @@
+D:/dest/3125/robei/verilog/arm.v
+D:/dest/3125/robei/verilog/ble_ctrl.v
+D:/dest/3125/robei/verilog/car_top.v
+D:/dest/3125/robei/verilog/dht11_drive.v
+D:/dest/3125/robei/verilog/encoder.v
+D:/dest/3125/robei/verilog/lcd_display.v
+D:/dest/3125/robei/verilog/lcd_driver.v
+D:/dest/3125/robei/verilog/lcd_rgb_char.v
+D:/dest/3125/robei/verilog/motor.v
+D:/dest/3125/robei/verilog/motor_contr.v
+D:/dest/3125/robei/verilog/motor_controller.v
+D:/dest/3125/robei/verilog/pid_controller.v
+D:/dest/3125/robei/verilog/pwm_10kHz.v
+D:/dest/3125/robei/verilog/pwm_20ms.v
+
